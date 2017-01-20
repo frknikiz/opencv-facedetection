c79b77a0e0e06c079f4cae8806b4a9b28dda4ac8
frknikiz <frknikiz@gmail.com>
2017-01-20 21:54:32 +0300

    - Maven dep added - Added cross platform support

--- src/main/java/com/opencv/springboot/OpencvApplication.java
+++ src/main/java/com/opencv/springboot/OpencvApplication.java
@@ -1,15 +1,14 @@
 package com.opencv.springboot;
 
-import org.opencv.core.Core;
+import nu.pattern.OpenCV;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OpencvApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OpencvApplication.class, args);
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OpencvApplication.class, args);
+        OpenCV.loadShared();
+    }
 }
